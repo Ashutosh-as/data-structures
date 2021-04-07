fafbee08ca8a770dee48a1311d0045e8909da16c
Ashutosh <60571903+Ashutosh-as@users.noreply.github.com>
2021-04-07 13:39:33 +0530

    Time: 32 ms (81.44%), Space: 27.5 MB (89.05%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -4,19 +4,20 @@ public:
         int start=0;
         int end=nums.size()-1;
         while(start<=end){
-        int mid= start+(end-start)/2;
-        if(target==nums[mid]){
-            return mid;
-        }
-        else if(target<nums[mid]){
-            end=mid-1;
+            int mid= start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+              end=mid-1;  
+            }
+                
+            }
             
-        }
-        else{
-            start=mid+1;
-        }
-       
-        }
         return -1;
+      
     }
 };
